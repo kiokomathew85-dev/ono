2394cb6ea40fb6d4598e03b2929b0d3e3c2f77d3
It's Mathew <kiokomathew85@gmail.com>
2026-04-03 10:18:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ function combineUsers(...args) {
     users: []
   };
 
-  args.forEach((userArray) => {
+  // Step 4: Merge Arrays using spread operator
+  args.forEach(userArray => {
     combinedObject.users = [...combinedObject.users, ...userArray];
   });
 
+  // Step 5: Get today's date in 'M/d/yyyy' format
+  
   combinedObject.merge_date = Date.today().toString('M/d/yyyy');
 
   return combinedObject;
